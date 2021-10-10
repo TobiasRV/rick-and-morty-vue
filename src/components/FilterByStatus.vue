5a241844075dce17a9eb9fddfcce878f52549d67
TobiasRV <trodriguez@timejobs.work>
2021-10-10 14:44:11 -0300

    v1.0.0

---
+++ src/components/FilterByStatus.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="filter">
+    <div class="item" @click="filter('')">
+      All
+    </div>
+    <div class="item alive" @click="filter('Alive')">
+      Alive
+    </div>
+    <div class="item dead" @click="filter('Dead')">
+      Dead
+    </div>
+    <div class="item unknown" @click="filter('unknown')">
+      Unknown
+    </div>
+  </div>
+</template>
+
+<script>
+import { useStore } from 'vuex'
+export default {
+  setup(){
+    const store = useStore();
+    
+    const filter = ((status) => {
+      store.dispatch('filterByStatus', status);
+    });
+
+    return { 
+      filter
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.filter {
+  width: 400px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  border-radius: 10px;
+  overflow: hidden;
+  .item {
+    padding: 1rem 0.5rem;
+    background-color: var(--background-card);
+    text-align: center;
+    cursor: pointer;
+    &:hover {
+      color: var(--text-orange);
+    }
+  }
+  .alive {
+    &:hover {
+      color: var(--text-green);
+    }
+  }
+  .dead {
+    &:hover {
+      color: var(--text-red);
+    }
+  }
+  .unknown {
+    &:hover {
+      color: var(--text-gray);
+    }
+  }
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,47 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    characters: [],
+    charactersFilters: [],
   },
   mutations: {
+    setCharacters(state, payload) {
+      state.characters = payload;
+    },
+    setCharactersFilters(state, payload) {
+      state.charactersFilters = payload;
+    },
   },
   actions: {
+    async getCharacters({ commit }) {
+      try {
+        const response = await fetch(
+          'https://rickandmortyapi.com/api/character'
+        );
+        const data = await response.json();
+        commit('setCharacters', data.results);
+        commit('setCharactersFilters', data.results);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    filterByStatus({ commit, state }, status) {
+      const results = state.characters.filter((character) => {
+        return character.status.includes(status);
+      });
+      commit('setCharactersFilters', results);
+    },
+    filterByName({commit, state}, name) {
+      const formatName = name.toLowerCase();
+      const filter = state.characters.filter((character) => {
+        const characterName = character.name.toLowerCase();
+        if(characterName.includes(formatName)) {
+          return character;
+        }
+      })
+      commit('setCharactersFilters', filter)
+    }
   },
-  modules: {
-  }
-})
+  modules: {},
+});
